1f41e7ce6bd24400b6bd77e0051e8a84c8ce26ac
wooczak <kacper.luczak99@gmail.com>
2026-04-21 18:10:39 +0200

    New sections, UI updates

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -4,3 +4,4 @@ export { default as Process } from "./Process/Process.astro";
 export { default as ProjectsSlider } from "./ProjectsSlider/ProjectsSlider.astro";
 export { default as StatsBanner } from "./StatsBanner/StatsBanner.astro";
 export { default as BlogOverview } from "./BlogOverview/BlogOverview.astro";
+export { default as ContactForm } from "./ContactForm/ContactForm.astro";
