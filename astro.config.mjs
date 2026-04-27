0350d32d03ed3383c6ad77286a7be0951b65461d
wooczak <kacper.luczak99@gmail.com>
2026-04-27 15:04:22 +0200

    Updating fonts etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,49 @@ import solidJs from "@astrojs/solid-js";
 export default defineConfig({
   fonts: [
     {
-      provider: fontProviders.google(),
-      name: "Source Sans 3",
-      cssVariable: "--font-source-sans-3",
-      weights: ["200", "300", "400", "500", "600", "700"],
+      provider: fontProviders.local(),
+      name: "Source Sans Pro",
+      cssVariable: "--font-source-sans-pro",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/SourceSansPro-ExtraLight.ttf.woff2"],
+            weight: "200",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SourceSansPro-Light.ttf.woff2"],
+            weight: "300",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SourceSansPro-Regular.ttf.woff2"],
+            weight: "400",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SourceSansPro-SemiBold.ttf.woff2"],
+            weight: "600",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SourceSansPro-Bold.ttf.woff2"],
+            weight: "700",
+            style: "normal",
+          }, 
+          {
+            src: ["./src/assets/fonts/SourceSansPro-Black.ttf.woff2"],
+            weight: "800 900",
+            style: "normal",
+          }
+        ]
+      }
     },
   ],
 
   site: "https://wooczak.github.io",
   base: "/systemdruk",
-  integrations: [icon(), solidJs({ devtools: true}), ],
+  integrations: [icon({
+    iconDir: "src/icons",
+  }), solidJs(), ],
 });
