43e96f638e21602bc13318fa650d12dba48ed76e
wooczak <kacper.luczak99@gmail.com>
2026-05-11 18:02:08 +0200

    Updating styles for Offer, Hero, Navigation, and Subheading sections with layout adjustments and new Subheading styles.

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -2,6 +2,7 @@ export { default as Hero } from "./Hero/Hero.astro";
 export { default as Offer } from "./Offer/Offer.astro";
 export { default as Process } from "./Process/Process.astro";
 export { default as ProjectsSlider } from "./ProjectsSlider/ProjectsSlider.astro";
+export { default as ClientsSlider } from "./ClientsSlider/ClientsSlider.astro";
 export { default as StatsBanner } from "./StatsBanner/StatsBanner.astro";
 export { default as BlogOverview } from "./BlogOverview/BlogOverview.astro";
 export { default as ContactForm } from "./ContactForm/ContactForm.astro";
